1c4f4f603f01b4461f6129e8f67ea3377ef14e6a
chungisugithub <chungisu@gmail.com>
2022-10-10 23:14:51 +0900

    composer require laravel/ui "^2.1" --dev

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/posts', 'PostsController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
