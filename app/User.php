abbdc05f5fbd0575ef12b37c5915f84129a48f82
chungisugithub <chungisu@gmail.com>
2022-10-11 00:08:29 +0900

    오타 HasApiTokens to HasApiToken

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
+use Laravel\Passport\HasApiToken;
 
 class User extends Authenticatable
 {
