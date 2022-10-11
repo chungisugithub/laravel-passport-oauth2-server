2ae0a0f6f415d351334269a89653d8beb284e6dc
chungisugithub <chungisu@gmail.com>
2022-10-11 14:24:53 +0900

    api/posts 라우터 주석처리

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/posts', 'Api\PostsController@index')
-    ->middleware(['auth:api', 'view-posts']);
+// Route::get('/posts', 'Api\PostsController@index')
+//     ->middleware(['auth:api', 'view-posts']);
