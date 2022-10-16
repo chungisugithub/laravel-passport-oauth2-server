a3c0023d437de73a81ca783a424fa03a446cca52
chungisugithub <chungisu@gmail.com>
2022-10-16 19:17:23 +0900

    업데이트 define 포스트

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,11 +2,16 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\User;
+use App\Post;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Post::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function() {
+            return factory(User::class)->create->id;
+        },
+        'title' -> $faker->sentence,
+        'body' -> $faker->paragraph
     ];
 });
